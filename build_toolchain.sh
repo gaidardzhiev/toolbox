ccd9129cb1245b573083e65e26d58257d864f1b9
gaidardzhiev <1v4n@protonmail.com>
2025-05-30 11:24:05 +0000

    Fri May 30 11:24:05 AM UTC 2025

--- build_toolchain.sh
+++ build_toolchain.sh
@@ -13,7 +13,7 @@ fgcc() {
 		return 0
 	} || {
 		cd "$SRCDIR" || return 1
-wget -c https://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VER.tar.gz || return 2
+		wget -c https://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VER.tar.gz || return 2
 		tar xf binutils-$BINUTILS_VER.tar.gz
 		rm binutils-$BINUTILS_VER.tar.gz
 		mkdir -p binutils-build
@@ -27,7 +27,7 @@ wget -c https://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VER.tar.gz || return
 			--target="$TARGET" || return 4
 		make $MAKEFLAGS all install || return 5
 		cd "$SRCDIR" || return 6
-wget -c https://ftp.gnu.org/gnu/gcc/gcc-$GCC_VER/gcc-$GCC_VER.tar.gz || return 7
+		wget -c https://ftp.gnu.org/gnu/gcc/gcc-$GCC_VER/gcc-$GCC_VER.tar.gz || return 7
 		tar xf gcc-$GCC_VER.tar.gz
 		rm gcc-$GCC_VER.tar.gz
 		cd gcc-$GCC_VER || return 8
@@ -38,13 +38,13 @@ wget -c https://ftp.gnu.org/gnu/gcc/gcc-$GCC_VER/gcc-$GCC_VER.tar.gz || return 7
 		../gcc-$GCC_VER/configure --prefix="$PREFIX" --disable-multilib --enable-languages=c,c++ --disable-bootstrap --disable-shared --enable-static --with-newlib --disable-libssp --disable-libquadmath --disable-libgomp --disable-libatomic --disable-libstdcxx-pch || return 12
 		make $MAKEFLAGS all-gcc all-target-libgcc install-gcc install-target-libgcc || return 13
 		cd "$SRCDIR" || return 14
-wget -c https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.5.1.tar.xz || return 15
+		wget -c https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.5.1.tar.xz || return 15
 		tar xf linux-6.5.1.tar.xz
 		rm linux-6.5.1.tar.xz
 		cd linux-6.5.1 || return 16
 		make ARCH=$(echo $TARGET | cut -d'-' -f1) INSTALL_HDR_PATH="$PREFIX/$TARGET" headers_install || return 17
 		cd "$SRCDIR" || return 18
-wget -c https://ftp.gnu.org/gnu/libc/glibc-$GLIBC_VER.tar.gz || return 19
+		wget -c https://ftp.gnu.org/gnu/libc/glibc-$GLIBC_VER.tar.gz || return 19
 		tar xf glibc-$GLIBC_VER.tar.gz
 		rm glibc-$GLIBC_VER.tar.gz
 		mkdir -p glibc-build
