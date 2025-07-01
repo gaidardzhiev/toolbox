57728d6052474ff448c66c14822212cbe90dbfa3
gaidardzhiev <1v4n@protonmail.com>
2025-07-01 07:40:21 +0000

    Tue Jul  1 07:40:21 AM UTC 2025

--- presentation.sh
+++ presentation.sh
@@ -20,15 +20,6 @@ SPEED=30
 	echo
 } | pv -qL $SPEED
 
-{
-	echo "Welcome to the presentation of the Toolbox project!"
-	echo
-	echo "Toolbox is a compact, statically linked binary that combines many common command line utilities into a single executable. It is designed as a minimalist and efficient toolkit that provides essential *nix commands, all accessible through one binary, simplifying deployment and usage especially in resource constrained or embedded environments."
-	echo
-	echo "Let's explore the toolbox commands and see them in action!"
-	echo
-} | pv -qL $SPEED
-
 FILE="./toolbox file toolbox | pv -qL 30"
 for ((i=0; i<${#FILE}; i++)); do
 	printf "%s" "${FILE:$i:1}"
