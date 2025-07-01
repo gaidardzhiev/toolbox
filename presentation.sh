327a8bf7a67fc2543af5c0f1ccf898438e16c0a8
gaidardzhiev <1v4n@protonmail.com>
2025-07-01 19:53:06 +0300

    Tue Jul  1 07:53:06 PM EEST 2025

--- presentation.sh
+++ presentation.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #ffmpeg -video_size 1920x1080 -framerate 30 -f x11grab -i :0.0 output.mp4
 #ffmpeg -i voice.m4a -i music.m4a -filter_complex "[0:a]volume=1.2[a0]; [1:a]volume=0.3[a1]; [a0][a1]amix=inputs=2:duration=longest[out]" -map "[out]" -c:a libmp3lame -q:a 2 output.mp3
-
+#ffmpeg -i output.mp4 -i output.mp3 -c:v copy -c:a aac -map 0:v:0 -map 1:a:0 presentation.mp4
 
 TITLE="Toolbox"
 NAME="Mr. Ivan Gaydardzhiev"
