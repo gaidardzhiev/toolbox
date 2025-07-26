19dff67ca93b91505fab36fc9e8a9463408f5acd
gaidardzhiev <1v4n@protonmail.com>
2025-07-26 09:20:50 +0000

    Sat Jul 26 09:20:50 AM UTC 2025

--- build_musl_libc.sh
+++ build_musl_libc.sh
@@ -14,7 +14,7 @@ export DIR="/usr/src/"
 
 fget() {
 	if cd "$DIR"; then
-		wget https://musl.libc.org/releases/$MUSL.tar.gz
+		wget https://musl.libc.org/releases/"$MUSL".tar.gz
 		tar xf $MUSL.tar.gz
 		rm $MUSL.tar.gz
 		cd $MUSL
