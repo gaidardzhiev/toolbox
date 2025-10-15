6d864417da5597b43337e3f7f9d361ddcce587ae
gaidardzhiev <1v4n@protonmail.com>
2025-10-15 13:02:08 +0000

    fix: quote variables to prevent word splitting and globbing

--- build_musl_libc.sh
+++ build_musl_libc.sh
@@ -3,23 +3,23 @@
 #Licensed under the GPL-3.0-only
 
 export PREFIX="/usr/local/musl"
-export BIN="$PREFIX"
-export LIB="$PREFIX"
+export BIN="${PREFIX}"
+export LIB="${PREFIX}"
 export GETNUMCPUS=`grep -c '^processor' /proc/cpuinfo`
-export JOBS='-j '$GETNUMCPUS''
+export JOBS='-j '${GETNUMCPUS}''
 export MUSL="musl-1.2.5"
 export DIR="/usr/src/"
 
-[ -d "$PREFIX" ] && { printf "%s exists...\n" "$PREFIX"; exit 0; }
+[ -d "${PREFIX}" ] && { printf "%s exists...\n" "${PREFIX}"; exit 0; }
 
-[ ! -d "$DIR" ] && mkdir -p "$DIR"
+[ ! -d "${DIR}" ] && mkdir -p "${DIR}"
 
 fget() {
-	if cd "$DIR"; then
-		wget https://musl.libc.org/releases/"$MUSL".tar.gz
-		tar xf "$MUSL".tar.gz
-		rm "$MUSL".tar.gz
-		cd "$MUSL"
+	if cd "${DIR}"; then
+		wget https://musl.libc.org/releases/"${MUSL}".tar.gz
+		tar xf "${MUSL}".tar.gz
+		rm "${MUSL}".tar.gz
+		cd "${MUSL}"
 		return 0
 	else
 		return 8
@@ -28,13 +28,13 @@ fget() {
 
 fbuild() {
 	if ./configure \
-		--prefix="$PREFIX" \
-		--exec-prefix="$BIN" \
-		--syslibdir="$LIB" \
+		--prefix="${PREFIX}" \
+		--exec-prefix="${BIN}" \
+		--syslibdir="${LIB}" \
 		--disable-shared; then
 		make
 		make install
-		cp "$BIN"/bin/musl-gcc /usr/bin/
+		cp "${BIN}"/bin/musl-gcc /usr/bin/
 		return 0
 	else
 		return 16
@@ -42,7 +42,7 @@ fbuild() {
 }
 
 ftry() {
-	if cd "$TMP"; then
+	if cd "${TMP}"; then
 		printf '#include <stdio.h>\nint main(int argc,char**argv){printf("hello %%d\\n",argc);}\n' > hello.c
 		musl-gcc -static -Os hello.c
 		./a.out
@@ -57,9 +57,9 @@ ftry() {
 	fi
 }
 
-{ fget && fbuild && ftry; RET=$?; } || {
+{ fget && fbuild && ftry; RET=${?}; } || {
 	printf "something's wrong in here somewhere...\n";
 	exit 1;
 }
 
-[ "$RET" -eq 0 ] 2>/dev/null || printf "%s\n" "$RET"
+[ "${RET}" -eq 0 ] 2>/dev/null || printf "%s\n" "${RET}"
